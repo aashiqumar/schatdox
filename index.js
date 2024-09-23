82c466b44e26e9c3995887892781a6b55bc98a58
Aashiq Umar <aash3q@gmail.com>
2024-09-23 22:14:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ wss.on('connection', (ws) => {
   });
 });
 
-// Start the server on port 4000
-const PORT = process.env.PORT || 4000;
+// Start the server on port 8080
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
